fba578c19321450eb1ed1e99be1ec040b24affee
Hatice <haticekaraty@gmail.com>
2021-02-18 21:58:08 -0500

    add required gems and files

---
+++ config/environment.rb
@@ -0,0 +1,8 @@
+require 'httparty'
+require 'rest_client'
+require 'pry'
+
+require_relative "../lib/space_expedition/version.rb"
+require_relative "../lib/space_expedition/api.rb"
+require_relative "../lib/space_expedition/expedition.rb"
+require_relative "../lib/space_expedition/cli.rb"
--- lib/space_expedition.rb
+++ lib/space_expedition.rb
@@ -1,7 +1,12 @@
 # frozen_string_literal: true
+require 'httparty'
+require 'rest_client'
+require 'pry'
 
-require_relative "space_expedition/version"
-
+require_relative "./space_expedition/version.rb"
+require_relative "./space_expedition/api.rb"
+require_relative "./space_expedition/expedition.rb"
+require_relative "./space_expedition/cli.rb"
 module SpaceExpedition
   class Error < StandardError; end
   # Your code goes here...
